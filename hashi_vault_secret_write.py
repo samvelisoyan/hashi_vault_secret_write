d9702876907071d59b68040bda53b31e2d2123b9
samvelisoyan <52713252+samvelisoyan@users.noreply.github.com>
2022-07-14 15:14:20 +0400

    added absent state

--- hashi_vault_secret_write.py
+++ hashi_vault_secret_write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 from ansible.module_utils.basic import *
 from jsonmerge import merge
 import requests
@@ -19,72 +18,71 @@ def update_secret(data, secret_url, session, module):
     else:
         module.fail_json(msg="faild to update secret: " + str(post_data.text))
 
+module = AnsibleModule(
+    argument_spec={
+        'vault_url': {'required': True},
+        'auth_method': {'default': 'ldap', 'choices': ['token', 'ldap',]},
+        'user': {'type': 'str'},
+        'token': {'type': 'str', 'no_log': True},
+        'password': {'type': 'str', 'no_log': True},
+        'mount_point': {'required': True},
+        'secret_path': {'required': True},
+        'data': {'type': 'dict', 'default': {} },
+        'validate_certs': {'type': 'bool', 'default': True, 'choices': [False, True]},
+        'state': {'default': 'present', 'choices': ['present', 'absent']}
+        },
+    supports_check_mode=True
+)
 
-def main():
+api_version = "v1"
+vault_url = module.params['vault_url']
+if vault_url.endswith('/'):
+    vault_url=vault_url[:-1]
 
-    module = AnsibleModule(
-        argument_spec={
-            'vault_url': {'required': True},
-            'auth_method': {'default': 'ldap', 'choices': ['token', 'ldap',]},
-            'user': {'type': 'str'},
-            'token': {'type': 'str', 'no_log': True},
-            'password': {'type': 'str', 'no_log': True},
-            'mount_point': {'required': True},
-            'secret_path': {'required': True},
-            'data': {'type': 'dict', 'default': {} },
-            'validate_certs': {'type': 'bool', 'default': True, 'choices': [False, True]},
-            'state': {'default': 'present', 'choices': ['present', 'absent']}
-            },
-        supports_check_mode=True
-    )
+auth_method = module.params['auth_method']
+token = module.params['token']
+user = module.params['user']
+password = module.params['password']
+mount_point = module.params['mount_point']
+secret_path = module.params['secret_path']
+if secret_path.startswith('/'):
+    secret_path=secret_path[1:]
 
-    api_version = "v1"
-    vault_url = module.params['vault_url']
-    if vault_url.endswith('/'):
-        vault_url=vault_url[:-1]
+data = module.params['data']
+validate_certs = module.params['validate_certs']
+state = module.params['state']
 
-    auth_method = module.params['auth_method']
-    token = module.params['token']
-    user = module.params['user']
-    password = module.params['password']
-    mount_point = module.params['mount_point']
-    secret_path = module.params['secret_path']
-    if secret_path.startswith('/'):
-        secret_path=secret_path[1:]
+api_url = vault_url + '/' + api_version
 
-    data = module.params['data']
-    validate_certs = module.params['validate_certs']
-    state = module.params['state']
+session = requests.Session()
+session.verify = validate_certs
 
-    api_url = vault_url + '/' + api_version
+if validate_certs == False:
+    from urllib3.exceptions import InsecureRequestWarning
+    urllib3.disable_warnings()
 
-    session = requests.Session()
-    session.verify = validate_certs
+if auth_method == 'ldap':
+    url_token = api_url + '/auth/ldap/login/' + user
+    auth = session.post(url_token, json={ 'password': password })
 
-    if validate_certs == False:
-        from urllib3.exceptions import InsecureRequestWarning
-        urllib3.disable_warnings()
+    if auth.status_code != 200:
+        module.fail_json(msg="authentication failed: " + str(auth.text))
 
-    if auth_method == 'ldap':
-        url_token = api_url + '/auth/ldap/login/' + user
-        auth = session.post(url_token, json={ 'password': password })
+    token = auth.json()['auth']['client_token']
 
-        if auth.status_code != 200:
-            module.fail_json(msg="authentication failed: " + str(auth.text))
+session.headers.update({'X-Vault-Token': token})
 
-        token = auth.json()['auth']['client_token']
+url_get = api_url + '/' + mount_point + '/data/' + secret_path
+get_vault = session.get(url_get)
 
-    session.headers.update({'X-Vault-Token': token})
-
-    url_get = api_url + '/' + mount_point + '/data/' + secret_path
-    get_vault = session.get(url_get)
-
+if state == 'present':
     if get_vault.status_code == 200:
         data_in_vault = json.loads(get_vault.text)['data']['data']
         final_data = merge(data_in_vault, data)
 
         if final_data == data_in_vault:
             module.exit_json(changed=False, found=True)
+
         else:
             update_secret(final_data, url_get, session, module)
 
@@ -94,4 +92,41 @@ def main():
     else:
         module.fail_json(msg="faild to get data from vault: " + str(get_vault.text))
 
-main()
+elif state == 'absent':
+    if get_vault.status_code == 200:
+        if data != {}:
+            final_data = {}
+            data_in_vault = json.loads(get_vault.text)['data']['data']
+
+            for key, value in data_in_vault.items():
+                if key not in data:
+                    final_data[key] = value
+
+            if final_data == {}:
+                vault_remove = session.delete(api_url + '/' + mount_point + '/metadata/' + secret_path)
+
+                if vault_remove.status_code in [200,204]:
+                    module.exit_json(changed=True, found=True)
+
+                else:
+                    module.fail_json(msg="faild to delete secret: " + str(vault_remove.text))
+
+            elif final_data == data_in_vault:
+                module.exit_json(changed=False, found=True)
+
+            else:
+                update_secret(final_data, url_get, session, module)
+
+        else:
+            vault_remove = session.delete(api_url + '/' + mount_point + '/metadata/' + secret_path)
+
+            if vault_remove.status_code in [200,204]:
+                module.exit_json(changed=True, found=True)
+
+            else:
+                module.fail_json(msg="faild to delete secret: " + str(vault_remove.text))
+
+
+    elif get_vault.status_code == 404:
+
+        module.exit_json(changed=False, found=True)
